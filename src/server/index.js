50c3e8b07bf9d400148c089df1edaa55666c3a15
Matthew Gordon <laxredemption5525@gmail.com>
2017-10-07 13:04:01 -0600

    Add router skeleton

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,10 @@
 const Koa = require('koa')
+const indexRoutes = require('./routes')
 
 const app = new Koa()
 const PORT = 1337
 
-app.use(async (ctx) => {
-  ctx.body = {
-    status: 'success',
-    message: 'hello, world!'
-  }
-})
+app.use(indexRoutes.routes())
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
